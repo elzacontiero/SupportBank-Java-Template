a8bfc77725dc412b42138a3636fbd8a0113218d2
Elza Contiero <elza.contiero@aexp.com>
2022-09-15 00:21:51 +0100

    Add Main class: still work in progress.

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,8 +1,24 @@
 package training.supportbank;
 
+import java.util.List;
+
 public class Main {
     public static void main(String args[]) {
-        // Your code here!
-        System.out.println("Test!");
+        System.out.println("Welcome to Support Bank");
+        List<Transaction> transactionList;
+        try {
+            transactionList = ReadTransactions.transactionsReader("Transaction2014.csv");
+        }
+        catch(Exception e) {
+            System.out.println("Error: "+e.getMessage()); // method inside exception provides error message.
+
+            return;// to quit application if there is error.
+        }
+
+        /* TODO: 1.	From the list of transactions create all accounts and save into a HashMap as I discussed with you today.
+                 2. Distribute transactions to each of the accounts.
+                 3. Loop for user input as per specification (List All or List [Account])
+         */
+
     }
 }
